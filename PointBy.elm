fb978edd63bc23ad89717c815cd1f63f58b220b6
Matthew Dupree <mattjdupree@gmail.com>
2018-04-02 23:07:48 -0700

    Rewrote countPoints with folds. Wrote folds. Began attributes.

--- PointBy.elm
+++ PointBy.elm
@@ -8,6 +8,7 @@ pointBy : Int -> PointBy attributearraytype
 pointBy numPoints =
     Point numPoints Nothing (pointBy <| numPoints-1)
 
+
 spendPoint : attributearraytype -> PointBy attributearraytype -> Result String (PointBy attributearraytype)
 spendPoint pointClass points = 
     case points of
@@ -17,6 +18,7 @@ spendPoint pointClass points =
             Result.Ok <| Point numRemaining (Just pointClass) remainingPoints
         Point _ (Just _) remainingPoints ->
             spendPoint pointClass remainingPoints
+
 
 unSpendPoint : attributearraytype -> PointBy attributearraytype -> Result String (PointBy attributearraytype)
 unSpendPoint pointClass points =
@@ -28,14 +30,43 @@ unSpendPoint pointClass points =
         Point _ _ remainingPoints ->
             unSpendPoint pointClass remainingPoints
 
-countPoints : attributearraytype -> PointBy attributearraytype -> Int
-countPoints pointClass points =
+
+foldr : (attributearraytype -> acc -> acc) -> acc -> PointBy attributearraytype -> acc
+foldr func acc points =
     case points of
         ZeroPoints ->
-            0
+            acc
         Point _ (Just pointClass) remainingPoints ->
-            (+) 1 <| countPoints pointClass remainingPoints
+            func pointClass (foldr (func) acc remainingPoints)
         Point _ _ remainingPoints ->
-            countPoints pointClass remainingPoints
+            foldr (func) acc remainingPoints
 
 
+foldl : (attributearraytype -> acc -> acc) -> acc -> PointBy attributearraytype -> acc
+foldl func acc points =
+    case points of
+        ZeroPoints ->
+            acc
+        Point _ (Just pointClass) remainingPoints ->
+            foldl func (func pointClass acc) remainingPoints
+        Point _ _ remainingPoints ->
+            foldl func acc remainingPoints
+
+
+countPoints : attributearraytype -> PointBy attributearraytype -> Int
+countPoints pointClass points =
+    let
+        check = (\point acc -> if point==pointClass then acc+1 else acc)
+    in
+        foldl check 0 points
+
+
+countRemainingPoints : PointBy attributearraytype -> Int
+countRemainingPoints points =
+    case points of
+        ZeroPoints -> 
+            0
+        Point _ (Nothing) remainingPoints ->
+            (+) 1 <| countRemainingPoints remainingPoints
+        Point _ _ remainingPoints ->
+            countRemainingPoints remainingPoints
