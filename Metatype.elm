f99c5b5a68448c67c0654c031fd40c71defbf9f9
Matthew Dupree <mattjdupree@gmail.com>
2018-04-03 12:49:52 -0700

    Completed basic metatype stats

--- Metatype.elm
+++ Metatype.elm
@@ -41,9 +41,17 @@ type TrollMetavariant
 
 
 
-getStats : Metatype -> (AttributeObject, AttributeObject)
-getStats metatype =
+getBaseStats : Metatype -> (AttributeObject, AttributeObject)
+getBaseStats metatype =
     case metatype of
+        Dwarf Nothing ->
+            ( AttributeObject 3 1 1 3 2 1 1 1 1 1 1
+            , AttributeObject 8 6 5 8 7 6 6 6 6 6 6
+            )
+        Elf Nothing ->
+            ( AttributeObject 1 2 1 1 1 1 1 3 1 1 1
+            , AttributeObject 6 7 6 6 6 6 6 8 6 6 6
+            )
         Human Nothing ->
             ( AttributeObject 1 1 1 1 1 1 1 1 2 1 1
             , AttributeObject 6 6 6 6 6 6 6 6 7 6 6
@@ -52,5 +60,13 @@ getStats metatype =
             ( AttributeObject 1 1 1 1 1 1 1 1 1 1 1
             , AttributeObject 6 6 6 6 6 6 6 6 6 6 6
             )
+        Ork Nothing ->
+            ( AttributeObject 4 1 1 3 1 1 1 1 1 1 1
+            , AttributeObject 9 6 6 8 6 5 6 5 6 6 6
+            )
+        Troll Nothing ->
+            ( AttributeObject 5 1 1 5 1 1 1 1 1 1 1
+            , AttributeObject 10 5 6 10 6 5 5 4 6 6 6
+            )
         _ ->
-            getStats (Human Nothing)
+            getBaseStats (Human Nothing)
