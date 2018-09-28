4a46d629199ed7c369fe224060c0103d3edbbb1f
Matthew Dupree <mattjdupree@gmail.com>
2018-09-28 13:45:22 +0200

    Make some stup parsing functions for Modifier.elm
    Rearrange the Modifier datastructure to fit within Elm's mapping limits.
    
    Correct bugs in structured-xml-decoder mapping types.

--- src/Modifier.elm
+++ src/Modifier.elm
@@ -1,27 +1,65 @@
 module Modifier exposing (Modifier,CanPurchase(..))
 
 import Dict exposing (Dict)
-import Xml.Decode as XD
+import Set exposing (Set)
+import Xml.Decode as XD exposing (XmlContentDecoder)
 
 type CanPurchase
     = CharGenOnly
     | CampaignOnly
     | Always
 
-
-type alias Modifier =
-    { id : String
-    , name : String
-    , karmaChange : Int
-    , canPurchase : CanPurchase
-    , limitTimesTaken : Int
-    , chargenEffects : Dict String Int
-    , mustHaves : List String
-    , mustNotHaves : List String
-    , effects : Dict String Int
-    , sourceBook : String
+type alias Sourcing =
+    { sourceBook : String
     , sourcePage : Int
     , nameOnPage : Maybe String
     }
 
---TODO: qualityToModifier test function
+type alias ModifierName = String
+
+type alias Id = String
+
+type alias Modifier =
+    { id : Id
+    , name : ModifierName
+    , effects : Effects
+    , requirements : Requirements
+    , source : Sourcing
+    }
+
+qualityModifier : XmlContentDecoder Modifier
+qualityModifier =
+    XD.map6 Modifier
+        <| XD.tag "id" XD.string
+        <| XD.tag "name" XD.string
+        <| qualityEffects
+        <| qualityRequirements
+        <| sourceIt
+
+type alias Effects =
+    { karmaChange : Int
+    , chargenEffets : Dict String Int
+    , alwaysEffects : Dict String Int
+    }
+
+qualityEffects : XmlContentDecoder Effects
+qualityEffects =
+    XD.succeed <| Effects 0 Dict.empty Dict.empty
+
+type alias Requirements = 
+    { canPurchase : CanPurchase
+    , maxTimesTaken : Maybe Int
+    , mustHaves : Set ModifierName
+    , mustNotHaves : Set ModifierName
+    }
+
+qualityRequirements : XmlContentDecoder Requirements
+qualityRequirements =
+    XD.succeed <| Requirements Always Nothing Set.empty Set.empty
+
+sourceIt : XmlContentDecoder Sourcing
+sourceIt =
+    XD.map3 Sourcing
+        (XD.tag "source" XD.string)
+        (XD.tag "page" XD.int)
+        (XD.maybeTag "nameonpage" XD.string)
